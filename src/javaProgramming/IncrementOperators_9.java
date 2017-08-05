5c8bd7bd7b2249ce03579991013cdcc46b20786f
Menja <menjaholm@gmail.com>
2017-08-05 15:08:02 +0200

    scanner added to all examples

--- src/javaProgramming/IncrementOperators_9.java
+++ src/javaProgramming/IncrementOperators_9.java
@@ -15,17 +15,18 @@ public class IncrementOperators_9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int horses = 5; 
-        int cows = 15;
-        int ducks = 128;
         
         /**HORSES: Pre increment adds 1 to the variable before it is using the variables value*/
+        System.out.println("How many horses do you have?");
+        int horses = scanner.nextInt(); 
         System.out.println("There are " + horses + " horses");
         ++horses;
         System.out.println("With pre increment (++horses) it adds 1 before it use the variable value.");
         System.out.println("So there are now " + horses + " horses" + "\n");
         
         /**COWS: Increment the operator after the variable*/
+        System.out.println("How many cows do you have?");
+        int cows = scanner.nextInt();
         System.out.println("There are " + cows + " cows");
         System.out.println("With increment (cows++) it adds 1 after it use the variable value.");
         System.out.println("So it will use the variable and then add 1 to it ");
@@ -34,18 +35,21 @@ public class IncrementOperators_9 {
         
         
         /**ASSIGNMENT OPARETOR*/
+        System.out.println("How many ducks do you have?");
+        int ducks = scanner.nextInt();
         /**Add more than 1 to the variable*/
         System.out.println("There are " + ducks + " ducks");
-        System.out.println("We buy 53 new ducks, how many are there now?");
+        System.out.println("We buy new ducks, how many are there now?");
         
         //one way to do it
-        ducks = ducks + 53;
-        System.out.println("With the new ducks we now have: " + ducks + " ducks" + "\n");
+        System.out.println("How many ducks did you buy?");
+        int newDucks = scanner.nextInt();
+        System.out.println("With the new ducks we now have: " + (ducks + newDucks) + " ducks" + "\n");
         
         //second way to do it
         System.out.println("Again we buy new ducks, this time 77 ducks.");
         ducks += 77;
-        System.out.println("Now we have: " + ducks + " ducks");
+        System.out.println("Now we have: " + (ducks + newDucks) + " ducks");
         
         
         
