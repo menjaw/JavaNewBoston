41a70efa814702e33574c7ab88f53f3eb6fa1a8e
Menja <menjaholm@gmail.com>
2017-08-06 12:02:23 +0200

    Logical operators added with & and || conditions

--- src/javaProgramming/LogicalOperators_11.java
+++ src/javaProgramming/LogicalOperators_11.java
@@ -5,10 +5,38 @@
  */
 package javaProgramming;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Menja
  */
 public class LogicalOperators_11 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader( new InputStreamReader(System.in));
+        
+        int boy, girl;
+        
+        System.out.println("How old is the boy?");
+        boy = Integer.parseInt(bf.readLine());
+        System.out.println(boy);
+        
+        System.out.println("How old is the girl?");
+        girl = Integer.parseInt(bf.readLine());
+        System.out.println(girl);
+        
+        
+        if (boy >= 15 && girl < 15 || boy < 15 && girl < 15) {
+            System.out.println("Sorry you are not allowed");
+        }
+        else if (boy < 15 && girl >= 15) {
+            System.out.println("Uh, you got a good story boy");
+        }
+        else {
+            System.out.println("Don't even ask, you are old enough");
+        }
+   
+    }
     
 }
